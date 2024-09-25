84140950c5030d370fee5dcc840c51faa2431616
Siddhartha singh <rajputsiddhartha2222@gmail.com>
2024-09-25 19:42:32 +0530

    set body parser

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const {PORT}=require('./config/serverconfig.js')
 
 const startAndSetupServer=async()=>{
     const app=express();
-const PORT=3000;
+    app.use(bodyParser.json);
+    app.use(bodyParser.urlencoded({extended:true}));
+
     app.listen(PORT,()=>{
         console.log(`server is running at ${PORT}`);
     })
