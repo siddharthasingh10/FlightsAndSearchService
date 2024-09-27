abbc2e252ccb0fc9e0d13d610bce51e2c5c76c3a
Siddhartha singh <rajputsiddhartha2222@gmail.com>
2024-09-27 23:20:30 +0530

    added some crud function

---
+++ README.md
@@ -0,0 +1,25 @@
+-clone the project on your local
+-Execute npm install on the same path as of your root directory of teh downloaded project
+-Create a .env file in the root directory and add the following environment variable
+      -PORT=3000
+-Inside the src/config folder create a new file config.json and then add the following piece of json
+```
+{
+  "development": {
+    "username": <YOUR_DB_LOGIN_NAME>,
+    "password": <YOUR_DB_PASSWORD>,
+    "database": "Flights_Search_DB_DEV",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
+```
+once you have added db cofig as listed above,go to the src  folder from your terminal and execute the `npx sequelize db:create` and then execute `npx sequelize db:migrate`
+
+
+
+### DB design
+     -airplane table
+     -flight table  
+     -Airport table
+     -city table 
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,15 +1,18 @@
+// all the models can be also accesed from ../models/index so we can also write const {City}=require('../models/index')
 const {City}=require('../models/city')
 
 class cityRepository{
-async createCity({name}){
+
+    async createCity({name}){
     try{
         const city=await City.create({name});
+        return city; 
     }
     catch(error){
+        console.log('somethinh happended wrong in repository level')
         throw {error};
     }
-}
-
+    }
     async deleteCity({cityId}){
         try{
                 await City.destroy({
@@ -17,13 +20,46 @@ async createCity({name}){
                         id:cityId
                     }
                 })
+                return true;
 
         }
         catch(error){
-                throw{error}
+            console.log('somethinh happended wrong in repository level')
+            throw {error};
+        }
+
+    }   
+    async getCity({cityId}){
+        try{
+            
+               const city= await City.findOne({
+                    where:{
+                        id:cityId
+                    }
+                })
+                return city;
+
+        }
+        catch(error){
+            console.log('somethinh happended wrong in repository level')
+            throw {error};
         }
 
     }
+    async updateCity(cityId,data){
+        try{
+                const city=await City.update(data,{
+                    where:{
+                        id:cityId
+                    }
+                })
+                    return city;
+        }
+        catch(error){
+            console.log('somethinh happended wrong in repository level')
+            throw {error};
+        }
 
+    }
 }
 module.exports=cityRepository;
